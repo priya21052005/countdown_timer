2bfaa28facf3ff3cbd9d5bcf693a82c205e8bed9
priya21052005 <pv0408288@gmail.com>
2024-05-16 12:11:39 +0530

    html file

---
+++ script.js
@@ -0,0 +1,18 @@
+var deadline = new Date("dec 8, 2023 15:37:25").getTime();
+
+var x = setInterval(function() {
+    var now = new Date().getTime();
+    var t = deadline - now;
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+    var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((t % (1000 * 60)) / 1000);
+    document.querySelector(".days").innerHTML =days ;
+    document.querySelector(".hours").innerHTML =hours;
+    document.querySelector(".minutes").innerHTML = minutes; 
+    document.querySelector(".seconds").innerHTML = seconds; 
+    if (t < 0) {
+        clearInterval(x);
+        document.getElementById("clockdiv").innerHTML = "EVENT HAS STARTED";
+    }
+}, 1000);
